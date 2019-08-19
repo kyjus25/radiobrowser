1e273b4346d1abd24360afc2440a07c07cbc9311
Justin White <kyjus25@gmail.com>
2019-08-19 10:33:15 -0500

    Add corsOptions

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const bodyParser = require('body-parser');
 const port = 4200;
 const child_process = require('child_process');
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://radio-browser.live',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
