eeb28e15c3d0fe0baef5b12816ba9901bde0a10f
Justin White <kyjus25@gmail.com>
2019-08-19 10:28:31 -0500

    Use cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const port = 4200;
 const child_process = require('child_process');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
